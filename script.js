40ce9e7b36e5c89c6a59f78d679a7aa54a0575d6
Pranav <pranavparasar321@gmail.com>
2024-04-07 23:10:24 +0530

    random pass generator

---
+++ script.js
@@ -0,0 +1,33 @@
+let btn = document.getElementById("btn");
+let passwordBox = document.getElementById("password");
+let copyBtn = document.getElementById("copyBtn");
+let length = 12;
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "~!@#$%^&*()_-{[}]|<>?";
+const combined = uppercase + lowercase + numbers + symbols;
+btn.onclick = () => {
+  generatePassword();
+};
+const generatePassword = () => {
+  let password = "";
+  password += uppercase[Math.floor(Math.random() * uppercase.length)];
+  password += lowercase[Math.floor(Math.random() * lowercase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+  length -= 4;
+  while (length >= 0) {
+    password += combined[Math.floor(Math.random() * combined.length)];
+    length--;
+  }
+  passwordBox.value = password;
+};
+
+copyBtn.onclick = () => {
+  copyPassword();
+};
+const copyPassword = () => {
+  passwordBox.select();
+  document.execCommand("copy");
+};
