3f489f068756e9a343934fe56e7c37f95bb1ec67
Pranav <pranavparasar321@gmail.com>
2024-04-07 23:19:53 +0530

    fixed password length issue

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btn = document.getElementById("btn");
 let passwordBox = document.getElementById("password");
 let copyBtn = document.getElementById("copyBtn");
-let length = 12;
+const length = 12;
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
@@ -16,10 +16,9 @@ const generatePassword = () => {
   password += lowercase[Math.floor(Math.random() * lowercase.length)];
   password += numbers[Math.floor(Math.random() * numbers.length)];
   password += symbols[Math.floor(Math.random() * symbols.length)];
-  length -= 4;
-  while (length >= 0) {
+  //don't use length -- -> first time 12 then 4
+  while (length >= password.length) {
     password += combined[Math.floor(Math.random() * combined.length)];
-    length--;
   }
   passwordBox.value = password;
 };
